a77dff17eb447bff2bcbbd8757de1076d69773fa
kolbasaegor <40235209+kolbasaegor@users.noreply.github.com>
2021-04-28 00:46:33 +0300

    Update Test.hs

--- Test.hs
+++ Test.hs
@@ -13,7 +13,7 @@ eventCreatorN'withoutOverlapping n | n >= 0 = [eventCreator (t) (t+1) ("SAMPLE_T
 								   | otherwise = []
 
 eventCreatorN'withOverlapping :: Int -> [Event Int [Char]]
-eventCreatorN'withOverlapping n | n >= 0 = [eventCreator (t `mod` 97) ( (2*t) `mod` 113) ("putin") | t <- [0 .. n-1] ]
+eventCreatorN'withOverlapping n | n >= 0 = [eventCreator (t `mod` 97) ( (2*t) `mod` 113) ("SAMPLE_TEXT") | t <- [0 .. n-1] ]
 				                | otherwise = []				
 
 benchmark1 = eventCreatorN'withoutOverlapping 40
